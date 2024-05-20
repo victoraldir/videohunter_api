5a1230b2ceba014c4693eaacb3324af768d770f5
Victor Hugo Montes Neves <vhaldir@gmail.com>
2024-05-20 22:22:34 +0200

    commenting out integration tests

--- videohunter-api/adapters/twitter/downloader_test.go
+++ videohunter-api/adapters/twitter/downloader_test.go
@@ -1,25 +1,25 @@
 package twitter
 
-import (
-	"net/http"
-	"testing"
+// import (
+// 	"net/http"
+// 	"testing"
 
-	"github.com/stretchr/testify/assert"
-)
+// 	"github.com/stretchr/testify/assert"
+// )
 
-func TestTwitterDownloaderRepository_DownloadVideo(t *testing.T) {
+// func TestTwitterDownloaderRepository_DownloadVideo(t *testing.T) {
 
-	// Arrange
-	url := "https://twitter.com/gunsnrosesgirl3/status/1791770018243367237"
-	httpClient := &http.Client{}
-	repo := NewTwitterDownloaderRepository(httpClient)
+// 	// Arrange
+// 	url := "https://twitter.com/gunsnrosesgirl3/status/1791770018243367237"
+// 	httpClient := &http.Client{}
+// 	repo := NewTwitterDownloaderRepository(httpClient)
 
-	// Act
-	video, _, err := repo.DownloadVideo(url)
+// 	// Act
+// 	video, _, err := repo.DownloadVideo(url)
 
-	// Assert
-	assert.Nil(t, err)
-	assert.NotNil(t, video)
-	assert.Equal(t, "https://pbs.twimg.com/amplify_video_thumb/1791768540497788928/img/Yzb1F-T6In9eI6EW.jpg", video.ThumbnailUrl)
+// 	// Assert
+// 	assert.Nil(t, err)
+// 	assert.NotNil(t, video)
+// 	assert.Equal(t, "https://pbs.twimg.com/amplify_video_thumb/1791768540497788928/img/Yzb1F-T6In9eI6EW.jpg", video.ThumbnailUrl)
 
-}
+// }
